ffe0752d558ca06f3fe4e2f7982ce429feb9afba
khairul habibie <96696443+khairulhabibie@users.noreply.github.com>
2023-05-22 02:29:46 +0000

    add fitur ubah bahasa dengan react context + localstorage

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,18 +3,28 @@ import { Link } from 'react-router-dom';
 import { FiHome, FiPlusCircle, FiLogOut } from 'react-icons/fi';
 import PropTypes from "prop-types"
 
+import { LocaleConsumer } from '../contexts/LocaleContext';
+
 function Navigation({ logout, name }) {
   return (
-    <nav className="navigation">
-      <ul>
-        <li><Link to="/"><FiHome /></Link></li>
-        <li><Link to="/add"><FiPlusCircle /></Link></li>
-        <li><button><FiLogOut onClick={logout} />{name}</button></li>
-      </ul>
-    </nav>
+    <LocaleConsumer>
+      {
+        ({ locale, toggleLocale }) => {
+          return (
+            <nav className="navigation">
+              <ul>
+                <li><button onClick={toggleLocale}>{locale === 'id' ? 'en' : 'id'}</button></li>
+                <li><Link to="/"><FiHome /></Link></li>
+                <li><Link to="/add"><FiPlusCircle /></Link></li>
+                <li><button onClick={logout}>{name} <FiLogOut /></button></li>
+              </ul>
+            </nav>
+          )
+        }
+      }
+    </LocaleConsumer>
   );
 }
-
 Navigation.propTypes = {
   logout: PropTypes.func.isRequired,
   name: PropTypes.string.isRequired,
